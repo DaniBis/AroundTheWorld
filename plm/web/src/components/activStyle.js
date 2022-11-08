53707737d7e6c39e1f7b07ebab2ba10f993bc99c
DaniBis <bisceanudaniel@gmail.com>
2022-11-08 11:09:54 +0700

    adding deploy script

--- plm/web/src/components/activStyle.js
+++ plm/web/src/components/activStyle.js
@@ -1,9 +1,17 @@
 import React from 'react';
+import './../style/activStyle.css';
 
 export default function activStyle() {
     return (
-        <div>
-            <h1>activStyle</h1>
+        <>
+        <div className='lockStock'>
+            <div>
+                <p>Humanity doesn't colectively make decisions, only people full of greed and powerful do make decisions for the whole humanity</p>
+            </div>
+            <div id="earth">
+            </div>
         </div>
+        </>
+
     )
 }
